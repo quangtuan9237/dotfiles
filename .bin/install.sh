8aeb30451d13c1f8c04f69dac61514b4e45c4bd3
Tuan Duong <tuanduong@Tuans-MacBook-Pro.local>
2022-12-27 01:43:46 +0700

    update install

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:quangtuan9237/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/quangtuan9237/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
