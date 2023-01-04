366d26358632e96620cecc20c2b78f6fd3b7db2b
Tuan Duong <quangtuan9237@gmail.com>
2023-01-04 19:08:14 +0700

    update install zsh

--- .bin/install-zsh.sh
+++ .bin/install-zsh.sh
@@ -6,9 +6,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 cp ~/.zshrc.orig ~/.zshrc
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
-
+chsh -s $(which zsh)
